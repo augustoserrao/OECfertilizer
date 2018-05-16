bf2544372f1edb25fd61e86bbbd09833218d50bc
Augusto Serrao <augusto_moya_serrao@hotmail.com>
2018-05-16 19:29:21 -0400

    [AKG_OEC] Initial commit with project created from scratch in Visual Studio.

---
+++ AKG_OEC/AKG_OEC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AKG_OEC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
